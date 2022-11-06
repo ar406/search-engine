d9f1e384b2289774ae6adcc28b2a7d46bb823f03
ar406 <66774262+ar406@users.noreply.github.com>
2022-11-06 11:02:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var sqlite3 = require('sqlite3').verbose();
 var express = require('express');
 var http = require('http');
 var path = require('path');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var helmet = require('helmet');
 
 var app = express();
@@ -16,32 +16,27 @@ app.use(helmet());
 //added to render html with answer!
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname));
-//end
 
 app.get('/', function(req,res) {
 	res.sendFile(path.join(__dirname,'search-engine.html'));
 });
 
 app.get('/result', function(req,res) {
-//function fetch_result(search,answer_callback) {}
 console.log(req.query.search)
 sanitized_query = req.query.search.replace(/[^A-Za-z0-9]/g, '')
-  db.get('SELECT url FROM results WHERE keyword IS ?', [sanitized_query], function(err,answer) {
+  db.all('SELECT url FROM results WHERE keyword IS ?', [sanitized_query], function(err,answer) {
 	if (err) {res.send('Error');console.error(err.message);}
-//else { callback(null, answer);}
-//console.log(answer);
 
+console.log(answer);
+console.log(answer.length);
 if(typeof answer == "undefined") {
     answer = "";
 }
 
 
-//before rendering: res.send(answer.url);
-res.render("result", { message: answer.url });
+res.render("result", { message: answer });
 });
-//});
-//res.send('hello');
-//res.send(answer);
+
 });
 
 server.listen(80,function(){ 
